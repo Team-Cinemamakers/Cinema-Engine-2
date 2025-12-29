723356c3921728025002d0679cd4044d05a0d7b7
BeastTornado <81200477+BeastTornado@users.noreply.github.com>
2025-12-28 23:57:40 -0500

    removed unnecessary comment

--- source/backend/CinemaGame.hx
+++ source/backend/CinemaGame.hx
@@ -7,8 +7,6 @@ import openfl.utils.Assets;
 class CinemaGame extends flixel.FlxGame
 {
     public override function switchState(){
-        //Clears all openfl asset cache (planning on removing this completely soon)
-        //Assets.cache.clear();
         //clears all from assettracking
         AssetTracking.clearAll();
         Gc.run(true);
